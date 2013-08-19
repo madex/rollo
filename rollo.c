37078e5f13a4944393ba3bfaec65e7c345cc92a9
Martin Ongsiek <martin.ongsiek@gmail.com>
2013-08-19 21:59:33 +0200

    kleine Änderung

--- rollo.c
+++ rollo.c
@@ -2,7 +2,7 @@
  * Konzeption
  * ==========
  *
- * Eingänge abfragen:
+ * EingŠnge abfragen:
  *  - Ablauf
  *    - ser clk !clk !ser rck_i !rck_i einlesen (stecker 1)
  *				 clk !clk rck_i !rck_i einlesen (stecker 2)
@@ -10,9 +10,9 @@
  *				 clk !clk rck_i !rck_i einlesen (stecker 4)
  *				 clk !clk rck_i !rck_i einlesen (stecker 5)
  *				 clk !clk rck_i !rck_i einlesen (stecker 6)
- *  - 36 Eingänge entprellen. Jedes Rollo (2 Eingänge) hat eine eigene 
+ *  - 36 EingŠnge entprellen. Jedes Rollo (2 EingŠnge) hat eine eigene
  *    Struct mit Timer und Zustand, GroupId und einen freiparametrierbaren Namen.
- *  - Die 18 Rollo Inputs können Events erzeugen. Up, Down, Stop ..
+ *  - Die 18 Rollo Inputs kšnnen Events erzeugen. Up, Down, Stop ..
  *  - Jedes Rollo hat eine GroupId. Mehrere RolloMotoren kšnnen mit 
  *    einer GroupId addressiert werden.
  *  - Neben den Eingängen kann die Uhr auch Events erzeugen, die 
@@ -22,21 +22,21 @@
  *  - An der Steuerung kšnnen auch fŸr alle Gruppen Events erzeugt werden.  
  *  - Die Gruppen beinhalten
  *    - einen parametrierbaren Namen (20 Zeichen)
- *    - eine Ausgangsbitmaske mit alle Ausgänge die angesprochen werden sollen.
+ *    - eine Ausgangsbitmaske mit alle AusgŠnge die angesprochen werden sollen.
  *    - eine Priorität? (Noch unklar wie sie funktionieren soll)
  *    - Es gibt maximal 30 Gruppen. (siehe unten)
  *    - Der GroupManager wertet entsprechend der Gruppe und der PrioritŠt die
  *      Events aus und leitet sie an die entsprechenden RolloControls weiter.
- *  - Es gibt 10 Rollos. Die restlichen 12 Relaisausgänge könnten andersweitig
+ *  - Es gibt 10 Rollos. Die restlichen 12 RelaisausgŠnge kšnnten andersweitig
  *    z.B. fŸr zeitschaltfunktionen benutzt werden.
  * Rollos Motoren betreiben. RolloControl
  *  - Die Rolladen sollen 
  *
- * - 10 ms Takt 
+ *  - 10 ms Takt 
  * 
- * Rollos   KüFe KüTü WZLi WZRe GäWC HWR BAD KiRe KiLi SZim
- * Gruppen  KüFe KüTü WZLi WZRe GäWC HWR BAD KiRe KiLi SZim
- *          Kü(KüFe KüTü) Wz(WZLi WZRe) Unten(KüFe KüTü WZLi WZRe GäWC HWR)
+ * Rollos   KŸFe KŸTŸ WZLi WZRe GŠWC HWR BAD KiRe KiLi SZim
+ * Gruppen  KŸFe KŸTŸ WZLi WZRe GŠWC HWR BAD KiRe KiLi SZim
+ *          KŸ(KŸFe KŸTŸ) Wz(WZLi WZRe) Unten(KŸFe KŸTŸ WZLi WZRe GŠWC HWR)
  *          Oben(BAD KiRe KiLi SZim) Alle(..)
  */
 
@@ -46,6 +46,7 @@
 
 unsigned long intputs_new, inputs_debounced;
 
+//
 typedef enum {
 	OFF,
 	ON,
@@ -53,6 +54,7 @@ typedef enum {
 	DOWN,
 } event_t;
 
+//
 typedef struct {
 	unsigned short timer;
 	unsigned char  groupId;
@@ -60,13 +62,15 @@ typedef struct {
 	event_t event;
 } intput_t;
 
+//
 typedef struct {
-	unsigned char days; // bit 7 mon bit 6 die ... 1 son
+	unsigned char days;           // bit 7 mon bit 6 die ... 1 son
 	unsigned char hour, min;
 	unsigned char groupId;
 	event_t event;
 } time_t;
 
+//
 typedef struct {
 	unsigned char days;           // bit 7 mon bit 6 die ... 1 son
 	unsigned char sonnenaufgang;  // 0 = sonnenuntergang
@@ -75,6 +79,7 @@ typedef struct {
 	event_t event;                // event
 } smart_time_t;
 
+//
 typedef struct {
 	unsigned long outputs;
 	char name[10];
